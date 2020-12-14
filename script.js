a48bce004bbdaecf2c424f332e5aa21b0370659a
FernanEd <fernandoed.bm@gmail.com>
2020-12-14 14:20:15 -0600

    1.0 design created

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("a");
+
+const CALCULATOR = {
+    '+': (num1, num2) => num1 + num2,
+    '-': (num1, num2) => num1 - num2,
+    '*': (num1, num2) => num1 * num2,
+    '/': (num1, num2) => num2 == 0? "Can't divide by 0": num1 / num2,
+}
+
